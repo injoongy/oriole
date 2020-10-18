0fc2ea2386cbfa812e634cfa0ba1c8ed015138cd
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2020-10-18 21:19:17 +0000

    Add user input prompt for token

--- commands/init.js
+++ commands/init.js
@@ -1,41 +1,21 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { render, useInput, useApp, Text, Box } from 'ink';
+import { Text, Box } from 'ink';
+import TextInput from 'ink-text-input';
 
 /// init command
 const init = () => {
-	const { exit } = useApp();
-	const [x, setX] = useState(1);
-	const [y, setY] = useState(1);
-
-	useInput((input, key) => {
-		if (input === 'q') {
-			exit();
-		}
-
-		if (key.leftArrow) {
-			setX(Math.max(1, x - 1));
-		}
-
-		if (key.rightArrow) {
-			setX(Math.min(20, x + 1));
-		}
-
-		if (key.upArrow) {
-			setY(Math.max(1, y - 1));
-		}
-
-		if (key.downArrow) {
-			setY(Math.min(10, y + 1));
-		}
-	});
+	const [token, setToken] = useState('');
 
 	return (
 		<Box flexDirection="column">
 			<Text>Please enter your Harvest Personal Access Token and your Harvest Account ID here.</Text>
 			<Text>Tokens and IDs can be found here: https://id.getharvest.com/oauth2/access_tokens/new</Text>
-			<Box height={12} paddingLeft={x} paddingTop={y}>
-				<Text>^_^</Text>
+			<Box>
+				<Box marginRight={1}>
+					<Text>Enter your Harvest Personal Access Token:</Text>
+				</Box>
+				<TextInput value={token} onChange={setToken} />
 			</Box>
 		</Box>
 	);
