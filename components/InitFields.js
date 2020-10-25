530d3eaebb1e9a383f89c2126f472ce5a53ae579
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2020-10-25 18:00:16 +0000

    Fix spacing

--- components/InitFields.js
+++ components/InitFields.js
@@ -4,57 +4,57 @@ import { Text, Box, useApp, useStdout } from 'ink';
 import TextInput from 'ink-text-input';
 
 const InitFields = () => {
-	const { exit } = useApp();
-	const { write } = useStdout();
+  const { exit } = useApp();
+  const { write } = useStdout();
 
-	const [token, setToken] = useState('');
-	const [accountId, setAccountId] = useState('');
-	const [showAccountField, setShowAccountField] = useState(false);
-	const [showBothFields, setShowBothFields] = useState(false);
+  const [token, setToken] = useState('');
+  const [accountId, setAccountId] = useState('');
+  const [showAccountField, setShowAccountField] = useState(false);
+  const [showBothFields, setShowBothFields] = useState(false);
 
-	const handleToken = value => setShowAccountField(true);
-	const handleAccountId = value => {
-		setShowBothFields(true); // write to file? Save to state? Follow my example in weatherbee using config.js?
-		exit();
-	}
+  const handleToken = value => setShowAccountField(true);
+  const handleAccountId = value => {
+  	setShowBothFields(true); // write to file? Save to state? Follow my example in weatherbee using config.js?
+  	exit();
+  }
 
-	return (
-		<Box flexDirection="column">
-			<Box marginTop={1}>
-				<Box marginRight={1}>
-					<Text color="green">❯</Text>
-				</Box>
-				<TextInput
-					placeholder="Enter your Harvest Personal Access Token here."
-					focus={!showAccountField}
-					showCursor={false}
-					value={token}
-					onChange={setToken}
-					onSubmit={handleToken}
-				/>
-			</Box>
-			{showAccountField && (
-				<Box>
-					<Box marginRight={1}>
-						<Text color="green">❯</Text>
-					</Box>
-					<TextInput
-						placeholder="Enter your Harvest Account ID here."
-						focus={showAccountField}
-						showCursor={false}
-						value={accountId}
-						onChange={setAccountId}
-						onSubmit={handleAccountId}
-					/>
-				</Box>
-			)}
-			{showBothFields && (
-				<Box marginTop={1}>
-					<Text>Token: {token}, ID: {accountId}</Text>
-				</Box>
-			)}
-		</Box>
-	);
+  return (
+  	<Box flexDirection="column">
+      <Box marginTop={1}>
+      	<Box marginRight={1}>
+          <Text color="green">❯</Text>
+      	</Box>
+      	<TextInput
+        	placeholder="Enter your Harvest Personal Access Token here."
+        	focus={!showAccountField}
+        	showCursor={false}
+        	value={token}
+        	onChange={setToken}
+        	onSubmit={handleToken}
+      	/>
+      </Box>
+    	{showAccountField && (
+      	<Box>
+        	<Box marginRight={1}>
+            <Text color="green">❯</Text>
+        	</Box>
+        	<TextInput
+          	placeholder="Enter your Harvest Account ID here."
+          	focus={showAccountField}
+          	showCursor={false}
+          	value={accountId}
+          	onChange={setAccountId}
+          	onSubmit={handleAccountId}
+        	/>
+      	</Box>
+    	)}
+    	{showBothFields && (
+        <Box marginTop={1}>
+          <Text>Token: {token}, ID: {accountId}</Text>
+        </Box>
+    	)}
+  	</Box>
+  );
 };
 
 export default InitFields;
