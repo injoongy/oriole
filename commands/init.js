4b981f4dca272ab6adbe629a9a434777827e9f49
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2020-10-25 17:51:01 +0000

    fix: Change spacing to use spaces so it looks good on GitHub

--- commands/init.js
+++ commands/init.js
@@ -5,18 +5,18 @@ import InitFields from '../components/InitFields';
 
 /// init command
 const init = () => {
-	return (
-		<Box flexDirection="column">
-			<Text>
-				Please enter your Harvest Personal Access Token and your Harvest Account ID here.
-			</Text>
-			<Text>
-				Tokens and IDs can be found at
-				<Text color="blue"> https://id.getharvest.com/oauth2/access_tokens/new</Text>.
-			</Text>
-			<InitFields />
-		</Box>
-	);
+  return (
+  	<Box flexDirection="column">
+  		<Text>
+			  Please enter your Harvest Personal Access Token and your Harvest Account ID here.
+  		</Text>
+  		<Text>
+  			Tokens and IDs can be found at
+  			<Text color="blue"> https://id.getharvest.com/oauth2/access_tokens/new</Text>.
+  		</Text>
+  		<InitFields />
+  	</Box>
+  );
 };
 
 export default init;
