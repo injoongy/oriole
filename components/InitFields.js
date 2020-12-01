3de5679f33e79ddc7004ac1362c987c6fe950917
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2020-12-01 05:21:20 +0000

    Use crypto to generate secret key and store it in keytar, and encrypt configstore with it

--- components/InitFields.js
+++ components/InitFields.js
@@ -3,20 +3,27 @@ import PropTypes from 'prop-types';
 import { Text, Box, useApp, useStdout } from 'ink';
 import TextInput from 'ink-text-input';
 import keytar from 'keytar';
+import crypto from 'crypto';
+import Conf from 'conf';
 
 const InitFields = () => {
   const { exit } = useApp();
   const { write } = useStdout();
+
+  const encryptionKey = crypto.randomBytes(256).toString('base64');
+  const config = new Conf({ encryptionKey });
 
   const [token, setToken] = useState('');
   const [accountId, setAccountId] = useState('');
   const [showAccountField, setShowAccountField] = useState(false);
   const [savedBothFields, setSavedBothFields] = useState(false);
 
-  const handleToken = value => setShowAccountField(true);
-  const handleAccountId = value => {
-    keytar.setPassword('oriole', 'token', token);
-    keytar.setPassword('oriole', 'accountId', accountId);
+  const handleToken = () => setShowAccountField(true);
+  const handleAccountId = () => {
+    keytar.setPassword('oriole', 'secret', encryptionKey);
+    config.set('token', token);
+    config.set('accountId', accountId);
+
     setSavedBothFields(true);
     exit();
   }
