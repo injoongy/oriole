e887e81d8905677c359d509d77d4e566269d6633
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2020-12-02 03:28:57 +0000

    Begin adding component for rendering project options

---
+++ components/SetOptions.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Text, Box, useApp } from 'ink';
+import TextInput from 'ink-text-input';
+import Conf from 'conf';
+
+const InitFields = () => {
+  const { exit } = useApp();
+
+  return (
+    <Box flexDirection="column"></Box>
+  )
+};
