e4f391c8f446be21c24ad118afea45e6eadd5d1a
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2020-12-27 23:02:36 +0000

    Update SetOptions component to use imported store methods

--- components/SetOptions.js
+++ components/SetOptions.js
@@ -2,16 +2,42 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Text, Box, useApp } from 'ink';
 import SelectInput from 'ink-select-input';
-import Conf from 'conf';
+import fetch from 'node-fetch';
+import { getData } from '../store';
 
 const SetOptions = () => {
   const { exit } = useApp();
+  const [userProjects, setUserProjects] = useState([]);
+
+  const buildOptions = async () => {
+    const token = await getData('token');
+    const accountId = await getData('accountId');
+
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Harvest-Account-ID': accountId,
+      },
+    };
+
+    return options;
+  };
+
+  const testFetch = async () => {
+    const options = await buildOptions();
+    const response = await fetch('https://api.harvestapp.com/v2/users/me', options);
+    const json = await response.json();
+    console.log('json', json);
+  };
+
+  testFetch();
 
   return (
     <Box flexDirection="column">
       <Text color="green">Set Options Here</Text>
     </Box>
-  )
+  );
 };
 
 export default SetOptions;
