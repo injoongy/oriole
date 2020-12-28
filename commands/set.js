28758b4fec4cf56fcf5f48010613efcd46bf83dc
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2020-12-28 02:11:25 +0000

    Clean up set component and add todos for SetOptions

--- commands/set.js
+++ commands/set.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Text, Box } from 'ink';
+import { Box } from 'ink';
 import SetOptions from '../lib/components/SetOptions';
 
 /// set command
 const set = () => {
   return (
     <Box flexDirection="column">
-      <Text>Please choose which Harvest project you want to link with the current directory.</Text>
       <SetOptions />
     </Box>
   );
