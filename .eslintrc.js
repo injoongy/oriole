2afa13ba5bfa6246f905f863bed09ae304afc98e
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2021-03-28 19:32:12 +0000

    Add eslint, tsconfig, and new Prettier config and install TS-related packages

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+const { join } = require('path');
+
+const tsconfigPath = join('.', '/tsconfig.json');
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  rules: {
+    semi: ['error', 'always'],
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+    // TODO: consider adding e.g. https://github.com/milesj/babel-plugin-typescript-to-proptypes
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+  },
+  parserOptions: {
+    project: tsconfigPath,
+    // https://github.com/typescript-eslint/typescript-eslint/issues/251
+    tsconfigRootDir: __dirname,
+  },
+};
