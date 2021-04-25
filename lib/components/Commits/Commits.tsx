3c7040440b013331c469f664b1ffcfa3254891fb
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2021-04-25 22:26:52 +0000

    🎉 Add logic to add a new Harvest entry with hardcoded hours

--- lib/components/Commits/Commits.tsx
+++ lib/components/Commits/Commits.tsx
@@ -2,20 +2,50 @@ import React, { FC, useState } from 'react';
 import { Text, Box, useApp } from 'ink';
 import SelectInput from 'ink-select-input';
 import * as child from 'child_process';
+import { getData } from '../../utils/store';
+import { pushHarvestEntry } from '../../utils/harvest/harvest';
+import { Error } from '../Error';
+import { HarvestError } from '../../utils/harvest/harvest.interface';
 import { Choice } from './Commits.interface';
 
 export const Commits: FC = () => {
   const { exit } = useApp();
   const [gitLog, setGitLog] = useState('');
   const [showGitLog, setShowGitLog] = useState(false);
+  const [error, setError] = useState<HarvestError>();
   const choices: Choice[] = [
     { label: 'Yes', value: 'y' },
     { label: 'No', value: 'n' },
   ];
-  const handleSelect = (item: Choice) => {
+  const currentDir = process.cwd().split('/');
+  const dirName = currentDir[currentDir.length - 1];
+
+  const handleSelect = async (item: Choice) => {
     if (item.value === 'y') {
-      console.log('confirmed');
-      exit();
+      const projectId = await getData(`${dirName}.projectId`);
+      const taskId = await getData(`${dirName}.taskId`);
+      // TODO: Allow user to add custom date as flag, --date yyyy-mm-dd?
+      const spentDate = new Date().toLocaleDateString('en-CA'); // today as yyyy-mm-dd
+      // TODO: Hours? Currently just hardcoded 3. Same question as above too - add as flag?
+      const hours = 3;
+      const body = {
+        project_id: Number(projectId),
+        task_id: Number(taskId),
+        spent_date: spentDate,
+        hours: hours,
+        notes: gitLog,
+      };
+      // TODO: Make sure this is idempotent. Currently can just keep running command to add multiple of the same entry.
+      pushHarvestEntry('https://api.harvestapp.com/v2/time_entries', body)
+        .then(() => {
+          console.log(`project: ${projectId}, task: ${taskId}`);
+          // TODO: Add logic to clear out the commits text and display a success message (similar logic to displaying error).
+          exit();
+        })
+        .catch((error) => {
+          setError(error);
+          exit();
+        });
     } else {
       console.log('rejected');
       exit();
@@ -27,24 +57,29 @@ export const Commits: FC = () => {
       'git log --author=$(git config user.email) --format="- %B" --no-merges -n 10',
     );
     const logString = log ? log.toString() : ''; // if log is null, like when exiting git log, there's an error - so ternary will resolve this
-    // TODO: need to get git log lines from whatever timeframe specified (default should be today, but since it's dev, just get the last 10),
-    // then format them with dashes and neat lines (handle multiline commits somehow?), and output that to the user for confirmation.
-    // Once confirmed, actually push it up to Harvest. Think about an option for a ticket heading?
+    // TODO: Think about an option for a ticket heading?
     const formattedLog = logString.replace(/(^[ \t]*\n)/gm, '');
     setGitLog(formattedLog);
     setShowGitLog(true);
   }
 
+  // TODO: Componentize everything to clean up ternary?
   return (
     <Box flexDirection='column'>
-      <Text>Here are your latest commits in this repo:</Text>
-      {showGitLog && (
-        <Box marginTop={1} flexDirection='column'>
-          <Text>{gitLog}</Text>
-          <Box flexDirection='column'>
-            <Text>Push these up to Harvest?</Text>
-            <SelectInput items={choices} onSelect={handleSelect} />
-          </Box>
+      {error && error.status ? (
+        <Error status={error.status} />
+      ) : (
+        <Box flexDirection='column'>
+          <Text>Here are your latest commits in this repo:</Text>
+          {showGitLog && (
+            <Box marginTop={1} flexDirection='column'>
+              <Text>{gitLog}</Text>
+              <Box flexDirection='column'>
+                <Text>Push these up to Harvest?</Text>
+                <SelectInput items={choices} onSelect={handleSelect} />
+              </Box>
+            </Box>
+          )}
         </Box>
       )}
     </Box>
