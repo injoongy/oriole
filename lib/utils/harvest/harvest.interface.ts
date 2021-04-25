54d349211a17b9db26b5ff8fbbaafe9150ba13e5
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2021-04-25 22:24:36 +0000

    🎉 Add new method for pushing a Harvest entry and update helper methods

--- lib/utils/harvest/harvest.interface.ts
+++ lib/utils/harvest/harvest.interface.ts
@@ -3,8 +3,17 @@ import { Headers } from 'node-fetch';
 export interface Options {
   method: string;
   headers: Headers;
+  body?: string;
 }
 
 export interface HarvestError {
   status?: number;
+}
+
+export interface HarvestEntry {
+  project_id: number;
+  task_id: number;
+  spent_date: string;
+  hours: number;
+  notes: string;
 }
--- lib/utils/harvest/harvest.ts
+++ lib/utils/harvest/harvest.ts
@@ -1,29 +1,44 @@
 import fetch, { Headers } from 'node-fetch';
 import { getData } from '../store';
-import { Options } from './harvest.interface';
+import { Options, HarvestEntry } from './harvest.interface';
 
-const buildOptions = async () => {
+const buildOptions = async (method: string, body?: string) => {
   const token = await getData('token');
   const accountId = await getData('accountId');
 
   const headers = new Headers();
   headers.set('Authorization', `Bearer ${token}`);
   headers.set('Harvest-Account-ID', accountId);
+  headers.set('Content-Type', 'application/json');
 
   const options: Options = {
-    method: 'GET',
+    method,
     headers,
+    body,
   };
 
   return options;
 };
 
 export const getHarvestData = async (url: string) => {
-  const options = await buildOptions();
+  const options = await buildOptions('GET');
   const response = await fetch(url, options);
   if (!response.ok) {
     throw { status: response.status };
   } else {
     return await response.json();
+  }
+};
+
+export const pushHarvestEntry = async (
+  url: string,
+  entryData: HarvestEntry,
+) => {
+  const options = await buildOptions('POST', JSON.stringify(entryData));
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    throw { status: response.status };
+  } else {
+    return response.ok;
   }
 };
