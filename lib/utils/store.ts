e972ffb0541ed52c6f737e11666fe9f99233221c
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2021-05-30 19:19:49 +0000

    Update let to const per ESLint's suggestion

--- lib/utils/store.ts
+++ lib/utils/store.ts
@@ -16,34 +16,34 @@ export const getEncryptionKey = async () => {
 
 export const getStore = async () => {
   const encryptionKey = await getEncryptionKey();
-  const store = new Conf({ encryptionKey, configName });
+  const store = new Conf({ configName });
   return store;
 };
 
 // TODO: refactor these methods if possible?
 export const saveData = async (key: string, data: string | number) => {
-  let store = await getStore();
+  const store = await getStore();
   store.set(key, data);
 };
 
 export const getData = async (key: string) => {
-  let store = await getStore();
+  const store = await getStore();
   return store.get(key) as string;
 };
 
 export const hasData = async (key: string) => {
-  let store = await getStore();
+  const store = await getStore();
   return store.has(key) as boolean;
 };
 
 export const deleteData = async (key: string) => {
-  let store = await getStore();
+  const store = await getStore();
   store.delete(key);
   return `${key} was deleted from the store.`;
 };
 
 export const deleteAllData = async () => {
-  let store = await getStore();
+  const store = await getStore();
   store.clear();
   return 'The entire store has been deleted.';
 };
