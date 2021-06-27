f40e7941bfb9c807bce21cd47e122c8a482a6688
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2021-06-27 20:34:07 +0000

    🎉 Add hours propType to push command

--- commands/push.tsx
+++ commands/push.tsx
@@ -1,14 +1,22 @@
 import React, { FC } from 'react';
+import PropTypes from 'prop-types';
 import { Box } from 'ink';
 import { Commits } from '../lib/components/Commits';
+import { CommitsProps } from '../lib/components/Commits/Commits.interface';
 
 /// push command
-const push: FC = () => {
-  return (
-    <Box flexDirection='column'>
-      <Commits />
-    </Box>
-  );
+const push: FC<CommitsProps> = ({ hours }) => (
+  <Box flexDirection='column'>
+    <Commits hours={hours} />
+  </Box>
+);
+
+push.propTypes = {
+  hours: PropTypes.number,
+};
+
+push.defaultProps = {
+  hours: 3,
 };
 
 export default push;
--- lib/components/Commits/Commits.interface.ts
+++ lib/components/Commits/Commits.interface.ts
@@ -1,3 +1,7 @@
+export interface CommitsProps {
+  hours?: number;
+}
+
 export interface Choice {
   label: string;
   value: string;
