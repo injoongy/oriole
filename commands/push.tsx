65302d41ba3ec9026ed440840a69f51b43b9f406
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2021-09-06 16:10:33 -0700

    🎉 Add specific date feature for push command

--- commands/push.tsx
+++ commands/push.tsx
@@ -5,20 +5,23 @@ import { Commits } from '../lib/components/Commits';
 import { PushProps } from '../lib/utils/commandTypes.interface';
 
 /// Create a timesheet entry out of the most recent git commit messages in this repo (for commits made today, local time) and push it up to Harvest.
-const push: FC<PushProps> = ({ hours }) => (
+const push: FC<PushProps> = ({ hours, date }) => (
   <Box flexDirection='column'>
-    <Commits hours={hours} />
+    <Commits hours={hours} commitDate={date} />
   </Box>
 );
 
 push.propTypes = {
   /// Set the hours spent on this Harvest entry.
   hours: PropTypes.number.isRequired,
+  /// Get the commits made on the specified date.
+  date: PropTypes.string,
 };
 
 // @ts-ignore: shortFlags property is a Pastel built-in property, not something for the push component/CommitsProps interface definition
 push.shortFlags = {
   hours: 'h',
+  date: 'd',
 };
 
 export default push;
--- lib/components/Commits/Commits.tsx
+++ lib/components/Commits/Commits.tsx
@@ -4,6 +4,7 @@ import SelectInput from 'ink-select-input';
 import * as child from 'child_process';
 import { getData } from '../../utils/store';
 import { pushHarvestEntry, getHarvestData } from '../../utils/harvest/harvest';
+import { isValidDate } from '../../utils/helpers';
 import { Error } from '../Error';
 import {
   HarvestError,
@@ -14,6 +15,7 @@ import { CommitsProps, Choice, EntryData, ExistingEntryData } from './Commits.in
 
 export const Commits: FC<CommitsProps> = ({ hours, commitDate }) => {
   const { exit } = useApp();
+  const [formattedDate, setFormattedDate] = useState('');
   const [gitLog, setGitLog] = useState('');
   const [showGitLog, setShowGitLog] = useState(false);
   const [existingEntry, setExistingEntry] = useState<ExistingEntryData>({});
@@ -31,7 +33,23 @@ export const Commits: FC<CommitsProps> = ({ hours, commitDate }) => {
   ];
   const currentDir = process.cwd().split('/');
   const dirName = currentDir[currentDir.length - 1];
-  const spentDate = new Date().toLocaleDateString('en-CA'); // today as yyyy-mm-dd
+
+  useEffect(() => {
+    if (!commitDate) {
+      // get date as YYYY-MM-DD, in a way that's compatible with Node v12 and older
+      // see this answer for why we can't do this a simpler way, like using toLocaleDateString:
+      // https://stackoverflow.com/a/56624712
+      // Format the month and force it to always be two digits: https://stackoverflow.com/a/47774150
+      const spentMonth = `${new Date().getMonth() + 1}`.padStart(2, '0');
+      // Format the day and force it to always be two digits: https://stackoverflow.com/a/47774150
+      const spentDay = `${new Date().getDate()}`.padStart(2, '0');
+      setFormattedDate(`${new Date().getFullYear()}-${spentMonth}-${spentDay}`);
+    } else if (commitDate && !isValidDate(commitDate)) {
+      setSuccess('Please enter a valid date, formatted as "YYYY-MM-DD".');
+    } else {
+      setFormattedDate(commitDate);
+    }
+  }, [formattedDate]);
 
   const checkDirInit = async () => {
     const token = await getData('token');
@@ -54,15 +72,26 @@ export const Commits: FC<CommitsProps> = ({ hours, commitDate }) => {
       if (res !== true) {
         setSuccess(res);
       } else {
-        const log = child
-          .execSync(
-            'git log --author=$(git config user.email) --format="- %B" --no-merges --since=midnight --reverse',
-          )
-          .toString();
-        // if there's no git log (aka no commits were made today), show message and exit
+        let log = '';
+        if (commitDate) {
+          // if date has been specified, get all the commits just on that day
+          log = child
+            .execSync(
+              `git log --author=$(git config user.email) --format="- %B" --no-merges --after="${commitDate} 00:00" --before="${commitDate} 23:59" --reverse`,
+            )
+            .toString();
+        } else {
+          // if no date has been specified, just grab the log from today
+          log = child
+            .execSync(
+              'git log --author=$(git config user.email) --format="- %B" --no-merges --since=midnight --reverse',
+            )
+            .toString();
+        }
+        // if there's no git log (aka no commits were made today or on the specified date), show message and exit
         if (!log) {
           setSuccess(
-            'No valid commits found.\nCommits need to have been made today in order to be considered valid.\nMerge commits are not considered valid.',
+            `No valid commits found for ${commitDate || 'today'}.\n(Merge commits are not considered valid.)`,
           );
           // else, format the outputted git log and set it as the gitLog variable value
         } else {
@@ -96,7 +125,7 @@ export const Commits: FC<CommitsProps> = ({ hours, commitDate }) => {
       return;
     }
     // Get all time entries for this date
-    getHarvestData(`https://api.harvestapp.com/v2/time_entries?from=${spentDate}`).then(
+    getHarvestData(`https://api.harvestapp.com/v2/time_entries?from=${formattedDate}`).then(
       (response) => {
         if (response.time_entries.length) {
           // See if any of the time entries are the same project and task
@@ -130,7 +159,7 @@ export const Commits: FC<CommitsProps> = ({ hours, commitDate }) => {
       const body = {
         project_id: projectId,
         task_id: taskId,
-        spent_date: spentDate,
+        spent_date: formattedDate,
         hours,
         notes: gitLog,
       };
@@ -149,7 +178,7 @@ export const Commits: FC<CommitsProps> = ({ hours, commitDate }) => {
     const body = {
       project_id: projectId,
       task_id: taskId,
-      spent_date: spentDate,
+      spent_date: formattedDate,
       hours,
       notes: gitLog,
     };
@@ -169,7 +198,7 @@ export const Commits: FC<CommitsProps> = ({ hours, commitDate }) => {
       const body = {
         project_id: projectId,
         task_id: taskId,
-        spent_date: spentDate,
+        spent_date: formattedDate,
         hours,
         notes: gitLog,
       };
@@ -187,14 +216,18 @@ export const Commits: FC<CommitsProps> = ({ hours, commitDate }) => {
           <Text>{success}</Text>
         </Box>
       ) : null}
-      {error && error.status ? (
+      {!success && error && error.status ? (
         <Box marginBottom={1}>
           <Error status={error.status} />
         </Box>
       ) : null}
       {!success && !error && !existingEntry.id && gitLog && showGitLog ? (
         <Box flexDirection='column'>
-          <Text>Here are your latest commits in this repo:</Text>
+          {commitDate ? (
+            <Text>Here are the commits made in this repo on {commitDate}:</Text>
+          ) : (
+            <Text>Here are your latest commits in this repo:</Text>
+          )}
           <Box marginTop={1} flexDirection='column'>
             <Text>{gitLog}</Text>
             <Box flexDirection='column'>
